6b7d391b016b31281715cf4585156553c17d0864
Paloma <palomadutra118@gmail.com>
2026-03-18 18:38:01 -0300

    atualizações

--- code/Game.py
+++ code/Game.py
@@ -1,19 +1,17 @@
 import pygame
 
+from code.Const import (WIN_WIDTH, WIN_HEIGHT)
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(800, 600))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run (self, ):
-        print('Setup Start')
 
-        print('Setup End')
 
-        print('Loop Start')
         while True:
             menu = Menu(self.window)
             menu.run()
@@ -21,10 +19,3 @@ class Game:
 
 
 
-                # Check for all events
-                # todos os eventos como fechar janela etc
-            #for event in pygame.event.get():
-             #   if event.type == pygame.QUIT:
-              #      print('Quitting...')
-               #     pygame.quit()  # close window
-                #    quit()  # end pygame
